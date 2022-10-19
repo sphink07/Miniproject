2e0cdaeb290ba5b60a23a16b1fe54d9aca938cbe
sphink07 <fromgun4455@gmail.com>
2022-10-19 11:49:34 +0700

    fix3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <v-app>
-      <v-alert border="right" type="error" elevation="2" :value="alertV">
-        Fusce commodo aliquam arcu. Pellentesque posuere. Phasellus tempus.
-        Donec posuere vulputate arcu.
+      <v-alert
+        style="z-index: 1; position: absolute; width: 100vw"
+        border="right"
+        type="error"
+        elevation="6"
+        :value="alertV"
+      >
+        Please select complete information.
       </v-alert>
-      <v-container style="text-align: center">
+      <v-container style="text-align: center; z-index: 0">
         <h1 style="color: gold; font-size: 50px">Truth Value Basics</h1>
         <span style="color: aliceblue; font-size: 20px">
           ──── Make by Nattanon ────
         </span>
-        <v-row class="mt-5">
+        <!-- ────────────────────────────────────────────────────────────── MD ────────────────────────────────────────────────────────────── -->
+        <v-row class="mt-5 hidden-sm-and-down">
           <v-col cols="4">
             <h1 style="color: gold">Variable (P)</h1>
             <v-col class="flex-center">
@@ -65,60 +71,202 @@
             </v-col>
           </v-col>
         </v-row>
-        <v-row>
+        <v-row class="hidden-sm-and-down">
           <v-container>
             <v-col>
-              <v-row>
-                <v-col>
-                  <p style="font-size: 50px" class="white--text">
-                    <span v-if="this.Pbox">~</span>
-                    P(
-                    <span style="font-size: 50px; color: gold">{{
-                      this.PVF
-                    }}</span>
-                    )
-                  </p>
-                </v-col>
-                <v-col>
-                  <p style="font-size: 50px" class="white--text">
-                    (
-                    <span style="font-size: 50px; color: gold">{{
-                      this.LVF
-                    }}</span>
-                    )
-                  </p>
-                </v-col>
-                <v-col>
-                  <p style="font-size: 50px" class="white--text">
-                    <span v-if="this.Qbox">~</span>
-                    Q(
-                    <span style="font-size: 50px; color: gold">{{
-                      this.QVF
-                    }}</span>
-                    )
-                  </p>
-                </v-col>
-              </v-row>
+              <v-container style="width: 50vw">
+                <v-row>
+                  <v-col>
+                    <p style="font-size: 40px" class="white--text">
+                      <span v-if="this.Pbox">~</span>
+                      P(
+                      <span style="font-size: 40px; color: gold">{{
+                        this.PVF
+                      }}</span>
+                      )
+                    </p>
+                  </v-col>
+                  <v-col>
+                    <p style="font-size: 40px" class="white--text">
+                      (
+                      <span style="font-size: 40px; color: gold">{{
+                        this.LVF
+                      }}</span>
+                      )
+                    </p>
+                  </v-col>
+                  <v-col>
+                    <p style="font-size: 40px" class="white--text">
+                      <span v-if="this.Qbox">~</span>
+                      Q(
+                      <span style="font-size: 40px; color: gold">{{
+                        this.QVF
+                      }}</span>
+                      )
+                    </p>
+                  </v-col>
+                </v-row>
+              </v-container>
             </v-col>
-            <v-col>
-              <v-btn @click="ClickBTN()" class="mr-6" width="100"
+            <v-col style="margin-top: 50px">
+              <v-btn
+                @click="ClickBTN()"
+                class="mr-10"
+                width="100"
+                style="
+                  color: mediumspringgreen;
+                  background-color: lightslategray;
+                "
                 >Confirm</v-btn
               >
-              <v-btn @click="reset()" width="100">{{ this.result }}</v-btn>
+              <v-btn
+                @click="reset()"
+                width="100"
+                style="color: linen; background-color: lightslategray"
+                >Reset</v-btn
+              >
             </v-col>
           </v-container>
         </v-row>
+        <!-- ────────────────────────────────────────────────────────────── SM ────────────────────────────────────────────────────────────── -->
+        <v-row class="mt-5 hidden-md-and-up">
+          <v-col cols="12">
+            <h1 style="color: gold">Variable (P)</h1>
+            <v-col class="flex-center">
+              <v-select
+                v-model="Pvarlue"
+                :items="items"
+                label="click here for select"
+                solo
+                style="width: 45vw"
+              >
+              </v-select>
+              <v-checkbox
+                v-model="Pbox"
+                style="margin-top: -10px"
+                class="black-label"
+                label="Negation ( ~ นิเสธ)"
+                color="#00e0ff"
+              ></v-checkbox>
+            </v-col>
+          </v-col>
+          <v-col style="margin-top:-15px" cols="12">
+            <h1 style="color: gold">Logic</h1>
+            <v-col class="flex-center">
+              <v-select
+                v-model="LogicValue"
+                :items="Logic"
+                label="click here for select"
+                solo
+                style="width:45vw"
+              >
+              </v-select>
+            </v-col>
+          </v-col>
+          <v-col cols="12">
+            <h1 style="color: gold">Variable (Q)</h1>
+            <v-col class="flex-center">
+              <v-select
+                v-model="Qvarlue"
+                :items="items"
+                label="click here for select"
+                solo
+                style="width: 45vw"
+              >
+              </v-select>
+              <v-checkbox
+                v-model="Qbox"
+                style="margin-top: -10px"
+                class="black-label"
+                label="Negation ( ~ นิเสธ)"
+                color="#00e0ff"
+              ></v-checkbox>
+            </v-col>
+          </v-col>
+        </v-row>
+        <v-row class="hidden-md-and-up">
+          <v-container>
+            <v-col>
+                <v-row>
+                  <v-col cols="5">
+                    <p style="font-size: 30px" class="white--text">
+                      <span v-if="this.Pbox">~</span>
+                      P
+                      <v-spacer></v-spacer>
+                      (
+                      <span style="font-size: 30px; color: gold">{{
+                        this.PVF
+                      }}</span>
+                      )
+                    </p>
+                  </v-col>
+                  <v-col cols="2">
+                    <p style="font-size: 30px" class="white--text">
+                      
+                      <span style="font-size: 30px; color: gold">{{
+                        this.LVF
+                      }}</span>
+                      
+                    </p>
+                  </v-col>
+                  <v-col cols="5">
+                    <p style="font-size: 30px" class="white--text">
+                      <span v-if="this.Qbox">~</span>
+                      Q
+                      <v-spacer></v-spacer>
+                      (
+                      <span style="font-size: 30px; color: gold">{{
+                        this.QVF
+                      }}</span>
+                      )
+                    </p>
+                  </v-col>
+                </v-row>
+            </v-col>
+            <v-col style="margin-top: 50px">
+              <v-btn
+                @click="ClickBTN()"
+                class="mr-10"
+                width="100"
+                style="
+                  color: mediumspringgreen;
+                  background-color: lightslategray;
+                "
+                >Confirm</v-btn
+              >
+              <v-btn
+                @click="reset()"
+                width="100"
+                style="color: linen; background-color: lightslategray"
+                >Reset</v-btn
+              >
+            </v-col>
+          </v-container>
+        </v-row>
+        <!-- ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── -->
       </v-container>
-      <v-overlay :z-index="0" :value="overlay" opacity="0.85">
-        <v-alert type="success" v-if="this.result == true"></v-alert>
+      <v-overlay
+        :z-index="0"
+        :value="overlay"
+        opacity="0.95"
+        @click="overlay = false"
+      >
+        <p style="text-align: center; font-size: 40px; font-weight: bold">
+          Your answer :
+        </p>
         <v-alert
+          style="font-size: 18px"
+          type="success"
+          v-if="this.result == true"
+          >TRUE</v-alert
+        >
+        <v-alert
+          style="font-size: 18px"
           type="error"
           icon="mdi-close-circle"
           v-if="this.result == false"
-        ></v-alert>
-        <v-btn class="white--text" color="teal" @click="overlay = false">
-          Hide Overlay
-        </v-btn>
+          >FALSE</v-alert
+        >
       </v-overlay>
     </v-app>
   </div>
@@ -145,31 +293,35 @@ export default {
       Qbox: false,
       result: null,
       overlay: false,
-      alertV:false,
+      alertV: false,
     };
   },
   methods: {
     ClickBTN() {
-      if(this.Pvarlue == null || this.Qvarlue == null || this.LogicValue == null){
-        this.alertV = true
+      if (
+        this.Pvarlue == null ||
+        this.Qvarlue == null ||
+        this.LogicValue == null
+      ) {
+        this.alertV = true;
         setTimeout(() => {
           this.alertV = false;
-            }, 1500);
+        }, 2000);
       } else {
         this.overlay = true;
-      switch (this.LogicValue) {
-        case "And(และ)":
-          this.AND();
-          break;
-        case "Or(หรือ)":
-          this.OR();
-          break;
-        case "Only if(ถ้า..แล้ว)":
-          this.IF();
-          break;
-        case "If and only if(ก็ต่อเมื่อ)":
-          this.Between();
-          break;
+        switch (this.LogicValue) {
+          case "And(และ)":
+            this.AND();
+            break;
+          case "Or(หรือ)":
+            this.OR();
+            break;
+          case "Only if(ถ้า..แล้ว)":
+            this.IF();
+            break;
+          case "If and only if(ก็ต่อเมื่อ)":
+            this.Between();
+            break;
         }
       }
     },
@@ -252,11 +404,15 @@ export default {
       }
     },
     reset() {
-      this.btnclick = false;
+      this.Pvarlue = null;
+      this.Qvarlue = null;
+      this.LogicValue = null;
+      this.Pbox = false;
+      this.Qbox = false;
+      this.PVF = null;
+      this.QVF = null;
+      this.LVF = null;
     },
-    Alert(){
-      
-    }
   },
   updated() {
     this.chackP();
